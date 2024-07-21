7e0368e51b84df244cfb44631f8bc1b12c6c0947
loshaniramsha <loshanikottagoda@gmail.com>
2024-07-21 11:56:25 +0530

    designed login page

---
+++ assests/script.js
@@ -0,0 +1,10 @@
+function setName(){
+    var name=document.getElementById('name').value;
+    if(name==""){
+        document.getElementById('error').style="display:block"
+    }else{
+        document.cookie="username="+name;
+        //console.log(document.cookie.split('=')[1]);
+        window.location.href = window.location.href.replace("/index.html","")+"/html/questions.html";
+    }
+}
